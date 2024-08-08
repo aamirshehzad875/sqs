26ab26b7fe98f88f1f6363a69e5db2b54efb6599
FaheemAzher <51700727+FaheemAzher@users.noreply.github.com>
2024-08-08 16:32:54 +0500

    sqs module

---
+++ main.tf
@@ -0,0 +1,34 @@
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "faheem-test"
+}
+
+locals {
+  sqs_messaging_queue_name = "sqs-${terraform.workspace}-messaging-notifications-queue-link"
+}
+
+module "sqs_messaging_notifications" {
+  source                    = "terraform-aws-modules/sqs/aws"
+  version                   = "4.0.1"
+  name                      = local.sqs_messaging_queue_name
+  message_retention_seconds = 1209600 # 14 days
+  max_message_size          = 262144  # 256 KB
+
+  tags = {
+    app       = "link social network"
+    env       = terraform.workspace
+    terraform = "true"
+  }
+
+}
+
+output "sqs_messaging_notifications_queue_url" {
+  value       = module.sqs_messaging_notifications.queue_url
+  description = "The URL of the SQS queue"
+}
+
+
+
+
+
